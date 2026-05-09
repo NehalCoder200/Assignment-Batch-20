0f2beeafe12b925588e90364ec0d17917c487b6e
Nehal Ali <nehalalikamran103@gmail.com>
2026-05-08 23:14:35 -0700

    Dom done

---
+++ Dom/app.js
@@ -0,0 +1,33 @@
+
+
+// console.log('javascript file chal rahi ha');
+
+
+let divTage = document.getElementById('prant1')
+
+// let length = divTage.childNodes.length - 1
+
+
+// console.log(divTage.childNodes);
+
+// console.log(divTage.childNodes[length]);
+
+
+// console.log(divTage.childNodes[1].TEXT_NODE);
+
+// console.log(divTage.childNodes[1].nodeType);
+
+
+// console.log(divTage.childNodes[1].nodeName);
+// console.log(divTage.childNodes[0].nodeName);
+
+// let target = divTage.hasAttribute('id')
+queueMicrotask``
+// console.log(target);
+
+
+// let target = divTage.getAttribute('class')
+
+divTage.childNodes[1].setAttribute("border", "1");
+
+// console.log(divTage.attributes); //nameNodeMape ka object de tha ha
