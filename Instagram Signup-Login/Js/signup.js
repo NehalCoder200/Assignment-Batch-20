d8c0025fd56fb16fcdae654214b2d1479be82b75
Nehal Ali <nehalcoder200@gmail.com>
2026-05-14 01:27:19 -0700

    work

---
+++ Instagram Signup-Login/Js/signup.js
@@ -0,0 +1,62 @@
+
+let Email = document.getElementById('email')
+let fullName = document.getElementById('fullName')
+let userName = document.getElementById('userName')
+let password = document.getElementById('password')
+
+
+function sweet(...rest) {
+    // console.log(rest[0]);
+
+    Swal.fire({
+        icon: rest[0],
+        title: rest[1],
+        text: rest[2],
+        footer: rest[3]
+    });
+}
+
+
+
+
+
+// console.log(Email);
+// console.log(fullName);
+// console.log(userName);
+// console.log(password);
+
+function signupHandler(e) {
+    e.preventDefault()
+
+    if (!Email.value.trim() ||
+        !fullName.value.trim() ||
+        !userName.value.trim() ||
+        !password.value.trim()  
+    ) {
+
+        return sweet('error', 'Oops', 'All Fileds Required ',
+            '<a href=\"#\">Why do I have this issue?</a>')
+    }
+
+    if(password.value.length < 8){
+        return sweet('error', 'Oops', 'Password 8 Character Required! ',
+            '<a href=\"#\">Why do I have this issue?</a>')
+    }
+    allUsers = []
+    
+    let users = {
+        email : Email.value,
+        fullName: fullName.value,
+        userName: userName.value,
+        password :password.value
+    }
+
+
+    allUsers.push(users)
+
+    console.log(users);
+    
+    console.log(allUsers);
+    
+
+}
