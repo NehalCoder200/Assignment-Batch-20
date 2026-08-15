18a63d5ce54f978ec98f8fd119f9b54e4c712998
Nehal Ali <nehalcoder200@gmail.com>
2026-08-15 02:21:24 -0700

    'react'

--- React/react-first-app/src/components/nav.jsx
+++ React/react-first-app/src/components/nav.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-let navbar = ['Home',"About",'Campuses','Check Result']
+let navbar = ['Home', "About", 'Campuses', 'Check Result']
 
 const nav = () => {
+
+    // var count = 0;
+    // let [count, setCount] = useState(0)
+
+
+    // var countFn = () => {
+    
+    //     console.log('run fn', count,);
+    
+    //     // count++
+    // }
+
+
+
+
+
     return <div className="nav">
         <h2>SMIT</h2>
 
         <ul className="nav-icons">
-            {navbar.map((items)=>{
-            return <li>{items}</li>
+            {navbar.map((items) => {
+                return <li>{items}</li>
             })}
         </ul>
 
-        <button>Enroll Now</button>
+        {/* <button onClick={() => setCount(count + 1)}>Enroll Now {count}</button> */}
+        {/* <button onClick={countFn}>Enroll Now {count}</button> */}
+        <button>Enroll Now </button>
 
     </div>
 
